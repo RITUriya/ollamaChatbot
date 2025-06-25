d3ddfd6874e177516b5c152b08b65be0bdb2132d
RITUriya <33688286+RITUriya@users.noreply.github.com>
2025-06-25 12:14:29 -0700

    ui code

---
+++ App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import React from "react";
+import Chatbot from "./Chatbot";
+
+function App() {
+  return (
+    <div className="moveForward">
+      <Chatbot />
+    </div>
+  );
+}
+
+export default App;
